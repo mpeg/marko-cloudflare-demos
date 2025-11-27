9e01bc2af7856d4971962d09a09146d3af801021
Marcos Perona <mperona@gmail.com>
2025-11-27 19:03:00 +0100

    feat: update marko-cloudflare config

--- marko-cloudflare/vite.config.ts
+++ marko-cloudflare/vite.config.ts
@@ -1,21 +1,10 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import marko from "@marko/vite";
 import { defineConfig } from "vite";
-import { readFile, writeFile } from "node:fs/promises";
 
 export default defineConfig({
     environments: {
         ssr: {
-            build: {
-                ssr: true,
-                target: "esnext",
-                outDir: 'dist',
-                rollupOptions: {
-                    output: {
-                        entryFileNames: '[name].js'
-                    }
-                }
-            },
             resolve: {
                 dedupe: ['marko'],
                 conditions: ['worker']
@@ -23,74 +12,28 @@ export default defineConfig({
             optimizeDeps: {
                 exclude: ['marko']
             }
-
         },
         client: {
             build: {
-                emptyOutDir: false,
-                outDir: 'dist/public',
                 modulePreload: {
                     polyfill: false
                 }
             }
         }
     },
+    builder: {
+		async buildApp(builder) {
+			const { ssr, client } = builder.environments;
+			if (!ssr || !client) {
+				throw new Error("Both ssr and client environments must be defined");
+			}
+			await builder.build(ssr);
+			await builder.build(client);
+		},
+	},
     publicDir: 'public',
     plugins: [
-        cloudflare({ viteEnvironment: { name: 'ssr' } }).map(p => {
-            return {
-                ...p, apply(config) {
-                    // we only want to apply the cloudflare plugin on the ssr build, otherwise it will break the client build, but apply on dev always
-                    if (config.mode === "development") {
-                        return true;
-                    }
-                    return Boolean(config?.build?.ssr)
-                }
-            }
-        }),
+        cloudflare({ viteEnvironment: { name: 'ssr' } }),
         marko({ linked: true }),
-        {
-            // this is needed as the cloudflare vite plugin is not expecting the linked build, and its buildApp hook hijacks the entry point on the ssr build if it finds a publicDir
-            // so it's a hack where we disable it on the ssr build, knowing it will still copy the public files in the client build
-            name: 'disable-public-dir-on-ssr-build',
-            enforce: 'pre',
-            config(config) {
-                if (config.build?.ssr) {
-                    return { ...config, publicDir: false }
-                }
-                return config;
-            },
-        },
-        {
-            name: 'fix-wrangler-assets-path',
-            enforce: 'post',
-            async buildApp() {
-                // hack to fix the built wrangler.json assets path, as the cloudflare vite plugin doesn't expect the linked build
-                try {
-                    const wrangler = JSON.parse(await readFile('dist/wrangler.json', 'utf-8'));
-                    wrangler.assets = { directory: 'public', binding: 'ASSETS' };
-                    await writeFile('dist/wrangler.json', JSON.stringify(wrangler));
-                    return;
-                }
-                catch {
-                    console.warn('Failed to update wrangler.json assets path');
-                }
-                
-            }
-        },
-        {
-			// prevent asset references from being included in the SSR manifest
-			// This is necessary because the cloudflare vite plugin will trigger
-			// a fallback client build if it finds any assets in the SSR manifest
-			name: "prevent-ssr-manifest-assets",
-			apply: (config) => Boolean(config.build?.ssr),
-			enforce: "post",
-			renderChunk(_code, chunk, _options) {
-				if (chunk.viteMetadata) {
-					chunk.viteMetadata.importedAssets = new Set();
-				}
-				return null;
-			},
-		},
     ]
 });
